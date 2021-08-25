aaf5131e04acc06b99c05f5646a6a7476afae393
Aleksandr Yailohanov <yalexer@yandex.ru>
2021-08-25 09:21:03 +0300

    validation

--- dirty_main.py
+++ dirty_main.py
@@ -1,6 +1,1 @@
-from application.salary import calculate_salary
-from application.db.people import get_employees
-
-if __name__ == '__main__':
-    print(calculate_salary())
-    print(get_employees())
+from application import *
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from datetime import datetime
+from application.salary import calculate_salary
+from application.db.people import get_employees
 
 data = datetime.now()
 
 if __name__ == '__main__':
    print(data)
+   print(calculate_salary())
+   print(get_employees())
